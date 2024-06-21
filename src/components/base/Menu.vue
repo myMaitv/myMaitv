3b10bbcfe44a4c7cc766c54e69bc385d552736b8
longvu19 <longvd.dev@gmail.com>
2024-06-22 06:15:26 +0700

    add logo and menu

---
+++ src/components/base/Menu.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import homeIcon from '/src/assets/home.svg?url';
+import movieIcon from '/src/assets/movie.svg?url';
+import tvIcon from '/src/assets/tv.svg?url';
+
+const menus = reactive([
+  {
+    name: 'Trang chủ',
+    path: '/',
+    icon: homeIcon
+  },
+  {
+    name: 'Phim bộ',
+    path: '/phim-bo',
+    icon: movieIcon
+  },
+  {
+    name: 'TV Shows',
+    path: '/tv-shows',
+    icon: tvIcon
+  }
+]);
+</script>
+
+<template>
+  <nav class="main-menu">
+    <ul class="main-menu__list">
+      <li class="main-menu__item" v-for="(menu, index) in menus" :key="index">
+        <a class="main-menu__link" :href="menu.path">
+          <span class="main-menu__icon" :style="{ maskImage: `url(${menu.icon})`, WebkitMaskImage: `url(${menu.icon})` }"></span>
+          <span>{{ menu.name }}</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss">
+.main-menu {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  &__list {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+    height: 100%;
+  }
+
+  &__item {
+    list-style: none;
+    height: 100%;
+  }
+
+  &__link {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    color: #fff;
+    font-weight: 400;
+    height: 100%;
+    font-size: 1.1rem;
+    gap: 10px;
+
+    .main-menu__icon {
+      display: block;
+      mask-size: 100%;
+      -webkit-mask-repeat: no-repeat;
+      mask-repeat: no-repeat;
+      mask-position: center;
+      height: 35px;
+      width: 35px;
+      background: white;
+    }
+
+    &:hover {
+      color: #bd4b1f;
+
+      .main-menu__icon {
+        background: #bd4b1f;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import Menu from '../base/Menu.vue';
+import { ref, onMounted, computed } from 'vue';
+import logo from '/logo-mytv.svg?url';
+
+const scrollTop = ref(0);
+const classObject = computed(() => {
+  return {
+    'header--bg-active': scrollTop.value > 0
+  }
+})
+onMounted(() => {
+  window.addEventListener('scroll', () => {
+    scrollTop.value = window.scrollY;
+  })
+})
+</script>
+<template>
+  <header class="header" :class="classObject">
+    <div class="header__left">
+      <div class="header__logo">
+        <img :src="logo" alt="logo">
+      </div>
+      <Menu />
+    </div>
+    <div class="header__right"></div>
+  </header>
+
+</template>
+<style lang="scss">
+.header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  display: flex;
+  padding: 0 60px;
+  align-items: center;
+  justify-content: space-between;
+  height: 70px;
+  background: rgb(0, 0, 0, 0);
+  transition: background 0.3s;
+  border-bottom: 1px solid #424242b0;
+
+  &--bg-active {
+    background: rgb(0, 0, 0, 0.4);
+  }
+
+  &__left,
+  &__right {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+    height: 100%;
+  }
+
+  &__logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+
+    img {
+      height: 100%;
+      width: auto;
+    }
+  }
+}
+</style>
