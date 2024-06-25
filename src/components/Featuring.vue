9cc17f0132073f00f541061a621ee2cb780fefbf
longvu19 <longvd.dev@gmail.com>
2024-06-25 14:29:27 +0700

    update searchbox and featuring css

--- src/components/Featuring.vue
+++ src/components/Featuring.vue
@@ -4,8 +4,10 @@ import { ref } from 'vue';
 
 <template>
   <div class="featuring">
-    <div class="featuring__background">
-      <img src="https://kenh14cdn.com/203336854389633024/2024/6/18/1-1433-1718632919958-1718632920090278823002-17186733436311358633289.jpg" alt="">
+    <div class="featuring__banner">
+      <div class="featuring__banner-slides">
+        <img src="https://kenh14cdn.com/203336854389633024/2024/6/18/1-1433-1718632919958-1718632920090278823002-17186733436311358633289.jpg" alt="">
+      </div>
     </div>
     <div class="featuring__title">
     </div>
@@ -17,7 +19,7 @@ import { ref } from 'vue';
   max-height: 100vh;
   position: relative;
 
-  &__background {
+  &__banner {
     position: absolute;
     top: 0;
     left: 0;
@@ -27,36 +29,53 @@ import { ref } from 'vue';
 
     &::after {
       content: '';
+      width: calc(100% + 2px);
+      height: 36%;
       position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      box-shadow: 0px -50px 43px -10px rgba(17, 17, 17, 1) inset;
-      -webkit-box-shadow: 0px -50px 43px -10px rgba(17, 17, 17, 1) inset;
-      -moz-box-shadow: 0px -50px 43px -10px rgba(17, 17, 17, 1) inset;
-      z-index: 0;
+      right: -1px;
+      bottom: -1px;
+      left: -1px;
+      z-index: 3;
+      background-image: linear-gradient(0deg, #111 0, rgba(17, 17, 17, 0) 82%);
     }
 
     &::before {
       content: '';
+      width: calc(100% + 2px);
+      height: 50%;
       position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      z-index: 1;
-      box-shadow: 0px 35px 43px -10px rgba(17, 17, 17, 0.5) inset;
-      -webkit-box-shadow: 0px 35px 43px -10px rgba(17, 17, 17, 0.5) inset;
-      -moz-box-shadow: 0px 35px 43px -10px rgba(17, 17, 17, 0.5) inset;
+      right: -1px;
+      bottom: -1px;
+      left: -1px;
+      z-index: 2;
+      background-image: linear-gradient(0deg, #111 0, rgba(17, 17, 17, 0) 100%);
     }
 
     img {
       width: 100%;
       height: 100%;
       object-fit: cover;
-      position: relative;
-      z-index: -1;
+    }
+  }
+
+  &__banner-slides {
+    position: relative;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+
+    &:before {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 4rem;
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      background-blend-mode: multiply;
+      mix-blend-mode: multiply;
+      background-image: linear-gradient(180deg, #111 0, rgba(17, 17, 17, 0) 100%);
     }
   }
 }
--- src/components/base/SearchBox.vue
+++ src/components/base/SearchBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-box">
     <form action="" method="get">
-      <InputBox id="searchInput" type="text" placeholder="Tìm kiếm"/>
+      <InputBox id="searchInput" type="text" placeholder="Tìm kiếm" />
     </form>
   </div>
 </template>
@@ -9,10 +9,20 @@
 import InputBox from './InputBox.vue';
 </script>
 
-<style lang="scss">
-#searchInput {
-  &::placeholder {
-    color: rgb(255, 255, 255) !important;
+<style lang="scss" scoped>
+
+.search-box {
+  :deep(#searchInput) {
+    &::placeholder {
+      color: rgb(255, 255, 255);
+    }
+  }
+  :deep(.input-box:not(:focus-within)) {
+    border-color: white;
+  }
+
+  :deep(.input-box__icon) {
+    background: #fff;
   }
 }
 </style>
